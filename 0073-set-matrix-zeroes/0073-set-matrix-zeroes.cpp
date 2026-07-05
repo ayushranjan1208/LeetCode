ae5df812a0877998e961eae8273297e3d0a57ae9
Ayush Ranjan <ayushranjan1208@gmail.com>
2026-07-05 16:07:59 +0530

    Time: 0 ms (100%), Space: 20.9 MB (50.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -7,8 +7,8 @@ public:
         for(int i = 0; i < m; i++) if(matrix[0][i] == 0) row1 = true;
         for(int i = 0; i < n; i++) if(matrix[i][0] == 0) col1 = true;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
                 if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
