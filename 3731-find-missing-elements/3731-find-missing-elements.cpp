cd8d185575c4adfbdf6e34e9650451fd8ede66b6
Ayush Ranjan <ayushranjan1208@gmail.com>
2026-08-04 18:27:18 +0530

    Time: 0 ms (100%), Space: 32.4 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
+
+        // TIME - O(N) SPACE - O(N)
+        // int n = nums.size();
+
+        // int mx = -1, mn = 101;
+
+        // for(auto e : nums){
+        //     mx = max(mx,e);
+        //     mn = min(mn,e);
+        // }
+
+        // vector<int> fre(mx - mn + 1);
+
+        // for(auto e : nums){
+        //     fre[e - mn] ++;
+        // }
+
+        // vector<int> ans;
+
+        // for(int i = 0; i < fre.size(); i++){
+        //     if(fre[i] == 0) ans.push_back(i + mn);
+        // }
+
+        // TIME - O(NLOGN) SPACE - O(1)
+
         int n = nums.size();
 
-        int mx = -1, mn = 101;
+        sort(nums.begin(),nums.end());
 
-        for(auto e : nums){
-            mx = max(mx,e);
-            mn = min(mn,e);
-        }
-
-        vector<int> fre(mx - mn + 1);
-
-        for(auto e : nums){
-            fre[e - mn] ++;
-        }
+        int mn = nums[0];
 
         vector<int> ans;
-
-        for(int i = 0; i < fre.size(); i++){
-            if(fre[i] == 0) ans.push_back(i + mn);
+        for(int i = 0; i < n; i++,mn++){
+            if(nums[i] > mn){
+                ans.push_back(mn);
+                i--;
+            }
         }
-
         return ans;
     }
 };
