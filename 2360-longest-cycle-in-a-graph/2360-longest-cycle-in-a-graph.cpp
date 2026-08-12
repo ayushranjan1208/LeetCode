314b24f2a5ce25d63d7d9115be6f270b3c6bc0ac
Ayush Ranjan <ayushranjan1208@gmail.com>
2026-08-13 01:26:46 +0530

    Time: 22 ms (83.43%), Space: 99.4 MB (86.41%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,33 +3,29 @@ public:
     vector<int> vis;
     int ans = -1;
 
-    void dfs(int node, int num, vector<vector<int>> &adj){
+    void dfs(int node, int num, vector<int> &edges){
+        if(node == -1) return;
+
         vis[node] = num;
-        for(int &ch : adj[node]){
-            if(vis[ch] == -1) continue;
-
-            if(vis[ch] != 0){
-                ans = max(ans, num - vis[ch] + 1);
-                continue;
-            }
-            dfs(ch, num + 1, adj);
+        int ch = edges[node];
+        if(ch != -1 && vis[ch] == 0){
+            dfs(ch, num + 1, edges);
         }
-
+        else{
+            if(ch != -1 && vis[ch] != -1){
+                ans = max(ans, num - vis[ch] + 1);
+            }
+        }
         vis[node] = -1;
     }
 
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<vector<int>> adj(n);
-        for(int i = 0; i < n; i++){
-            if(edges[i] == -1) continue;
-            adj[i].push_back(edges[i]);
-        }
 
         vis.assign(n,0);
         for(int i = 0; i < n; i++){
             if(!vis[i]){
-                dfs(i,1,adj);
+                dfs(i,1,edges);
             }
         }
 
