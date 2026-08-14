3d68b0cfbf89a232af7f7c73c16cfb5917835aa8
Ayush Ranjan <ayushranjan1208@gmail.com>
2026-08-14 22:21:37 +0530

    Time: 113 ms (50.95%), Space: 173.4 MB (79.4%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int cnt = 0;
+    long long dfs(int node, int par, vector<vector<int>> &adj, vector<int>& values, int k){
+        long long sum = 0;
+        for(auto &ch : adj[node]){
+            if(ch == par) continue;
+
+            sum += dfs(ch,node,adj,values,k);
+        }
+        sum += 1ll * values[node];
+        if(sum % k == 0){
+            cnt++;
+            sum = 0;
+        }
+        return sum;
+    }
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int>> adj(n);
+
+        for(auto &e : edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        dfs(0,-1,adj,values,k);
+
+        return cnt;
+    }
+};
